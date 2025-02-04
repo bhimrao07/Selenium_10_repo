7130ce10cd05c5a31c713654a0c9ef3a70a0f053
Kundan Sagar <118827781+kundansagarr8@users.noreply.github.com>
2025-02-04 11:11:44 +0530

    first commit

---
+++ SampleMaven_11/src/test/java/demo/ForkAndPullRequest.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ForkAndPullRequest {
+	@Test
+	public void sample()
+	{
+		Reporter.log("ForkAndPullRequest testcase passed",true);
+	}
+}
